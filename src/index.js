7e802d91f50ed0e293393ef6d67e0e04d54e089c
krishnaanaril <krishnamohan.a.m@gmail.com>
2018-11-29 16:47:20 +0530

    Connected server and database with prisma bindings

--- src/index.js
+++ src/index.js
@@ -1,58 +1,63 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
-// 1
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-},
-{
-    id: 'link-1',
-    url: 'www.krishnamohan.me',
-    description: 'My portfolio site'
-}]
+/*
+Not required since we've created a prisma database
+*/
+// // 1
+// let links = [{
+//     id: 'link-0',
+//     url: 'www.howtographql.com',
+//     description: 'Fullstack tutorial for GraphQL'
+// },
+// {
+//     id: 'link-1',
+//     url: 'www.krishnamohan.me',
+//     description: 'My portfolio site'
+// }]
 
-let idCount = links.length;
+// let idCount = links.length;
 
 /*
 */
 const resolvers = {
     Query: {
         info: () => 'This is the API of a HackerNews clone.',
-        feed: () => links
+        feed: (root, args, context, info) => {
+            return context.db.query.links({}, info);
+        }
     },
 
-    Link: {
-        id: (root) => root.id,
-        description: (root) => root.description,
-        url: (root) => root.url
-    },
+    // Link: {
+    //     id: (root) => root.id,
+    //     description: (root) => root.description,
+    //     url: (root) => root.url
+    // },
 
     Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link;
+        post: (root, args, context, info) => {
+            return context.db.mutation.createlink({
+                data: {
+                    url: args.url,
+                    description: args.description
+                }
+            }, info);
         },
 
-        updateLink: (root, args) => {
-            let index = links.findIndex((x)=> x.id === args.id);
-            links[index].url = args.url;
-            links[index].description = args.description;
-            return links[index]
-        },
+        // updateLink: (root, args) => {
+        //     let index = links.findIndex((x)=> x.id === args.id);
+        //     links[index].url = args.url;
+        //     links[index].description = args.description;
+        //     return links[index]
+        // },
 
-        deleteLink: (root, args) => {
-            let index = links.findIndex((x)=> x.id === args.id);
-            let link = links[index];
-            links.splice(index, 1)
-            return link;
+        // deleteLink: (root, args) => {
+        //     let index = links.findIndex((x)=> x.id === args.id);
+        //     let link = links[index];
+        //     links.splice(index, 1)
+        //     return link;
 
-        }
+        // }
     }
 }
 
@@ -60,7 +65,16 @@ const resolvers = {
 */
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://eu1.prisma.sh/krishna-mohan-36ae98/database/dev',
+            secret: 'mysecret123',
+            debug: true,
+        })
+    })
 });
 
 server.start(() => console.log('Server is running on http://localhost:4000'))
